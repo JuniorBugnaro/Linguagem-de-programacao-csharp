283f9492ef46a8f6e42169fcd275e04e8772d8d8
Junior Bugnaro <juninho.bugnaro@hotmail.com>
2022-11-27 08:36:16 -0300

    exercicios

---
+++ exercicios/Exercicio_5/Exercicio_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. Escreva um programa que receba 4 números e apresente a soma dos números pares e depois dos números impares.
+
+            float num = 0;
+            float soma_im = 0;
+            float soma_par = 0;
+
+            Console.WriteLine("Escreva quatro numeros: ");
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write("numero " + i + ": ");
+                float.TryParse(Console.ReadLine(), out num);
+                if (num % 2 == 0)
+                {
+                    soma_par += num;
+                } if (num % 2 == 1) 
+                {
+                    soma_im += num;
+                }
+            }
+            Console.WriteLine("A soma dos numeros pares é: " + soma_par);
+            Console.WriteLine("A soma dos numeros impares é: " + soma_im);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicios/Exercicio_6/Exercicio_6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/* 6. Escreva um programa que calcule a nota de cada aluno e a média da turma. O 
+        programa deve receber o nome do aluno e a nota de cada bimestre (4 notas), ao 
+        informar no nome do aluno o valor “finalizar” o programa deve realizar os cálculos da 
+        média e apresentar na tela */
+
+            float nota;
+            float soma = 0;
+            string aluno;
+            float mediaaluno = 0;
+            float mediaclasse = 0;
+            
+
+            Console.WriteLine("Quantidade de alunos na classe: ");
+            float quant = 0;
+            float.TryParse(Console.ReadLine(), out quant);
+            
+            for(int i = 1; i <= quant; i++)
+            {
+                Console.WriteLine("Nome do aluno: ");
+                aluno = Console.ReadLine();
+
+                for (int j = 1; j <= 4; j++)
+                {
+                    Console.WriteLine("Qual a nota do " + aluno + " No " + j + " Bimestre");
+                    float.TryParse(Console.ReadLine(), out nota);
+
+                    mediaaluno = nota / 4;
+                    soma += nota;
+                    mediaclasse = soma / quant;
+                
+                }
+            }
+            
+            Console.WriteLine("A soma das notas da classe é: " + soma);
+            Console.WriteLine("A media das notas da classe é: " + mediaclasse);
+
+            Console.ReadKey();
+        }   
+    }
+}
